491e3174ff273007c85ac822774f4c7f53df8f70
Ubuntu <ubuntu@ip-172-31-46-126.us-east-2.compute.internal>
2020-05-27 16:34:38 +0000

    A successful websocket based chat prototype

--- card_game_28/asgi.py
+++ card_game_28/asgi.py
@@ -23,22 +23,35 @@ async def application(scope, receive, send):
     else:
         raise NotImplementedError(f"Unknown scope type {scope['type']}")
 
-async def websocket_applciation(scope, receive, send):
+clients = {}
+client_id = 0
+
+async def websocket_application(scope, receive, send):
+    global clients
+    global client_id
+    my_id = 0
     while True:
         event = await receive()
 
         if event['type'] == 'websocket.connect':
+            client_id += 1
+            my_id = client_id
+            clients[my_id] = send
+            print("Connected: " + str(my_id))
             await send({
                 'type': 'websocket.accept'
             })
 
         if event['type'] == 'websocket.disconnect':
+            print("Disconnected: " + str(my_id))
+            clients.pop(my_id)
             break
 
         if event['type'] == 'websocket.receive':
-            if event['text'] == 'ping':
-                await send({
-                    'type': 'websocket.send',
-                    'text': 'pong!'
-                })
+            for id, send_fn in clients.items():
+                if id != my_id:
+                    await send_fn({
+                        'type': 'websocket.send',
+                        'text': event['text']
+                    })
 
