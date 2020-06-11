2438eaa77d3bd170917bc945aaa6b9489ce120c5
Ubuntu <ubuntu@ip-172-31-46-126.us-east-2.compute.internal>
2020-06-11 16:15:46 +0000

    WIP9 - Cleanup: Split css, js, py into files

--- card_game_28/asgi.py
+++ card_game_28/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from game_admin.websock_connection import websocket_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'card_game_28.settings')
 
@@ -22,36 +23,4 @@ async def application(scope, receive, send):
         await websocket_application(scope, receive, send)
     else:
         raise NotImplementedError(f"Unknown scope type {scope['type']}")
-
-clients = {}
-client_id = 0
-
-async def websocket_application(scope, receive, send):
-    global clients
-    global client_id
-    my_id = 0
-    while True:
-        event = await receive()
-
-        if event['type'] == 'websocket.connect':
-            client_id += 1
-            my_id = client_id
-            clients[my_id] = send
-            print("Connected: " + str(my_id))
-            await send({
-                'type': 'websocket.accept'
-            })
-
-        if event['type'] == 'websocket.disconnect':
-            print("Disconnected: " + str(my_id))
-            clients.pop(my_id)
-            break
-
-        if event['type'] == 'websocket.receive':
-            for id, send_fn in clients.items():
-                if id != my_id:
-                    await send_fn({
-                        'type': 'websocket.send',
-                        'text': event['text']
-                    })
 
